de2144a3a9452a0cc071a597ac060efe6479985e
uventura <ualivent@gmail.com>
2023-06-08 23:07:54 -0300

    Adding basic discord connection

---
+++ main.py
@@ -0,0 +1,43 @@
+import discord
+import re
+
+def add_movie(name):
+    movie_file = open("data/movies.txt", "a")
+    movie_file.write(name + "\n")
+
+def list_movies():
+    pass
+
+
+################################################
+################################################
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    msg_content = message.content
+
+    match = re.search(r"ch.add (.*)", msg_content)
+    if match:
+        movie = match.group(1)
+        add_movie(movie)
+
+        adding_msg = f"Adding movie {movie}..."
+        await message.channel.send(adding_msg)
+
+    #( Add list functionality )
+    match = re.search(r"ch.list", msg_content)
+    if match:
+        list_movies()
+
+client.run("TOKEN")
