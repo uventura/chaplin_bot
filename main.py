f2c0a45c4b12fd8ca43458385178f26224b9406e
uventura <ualivent@gmail.com>
2023-06-20 23:21:25 -0300

    Adding seen functionality
    This functionality enables the possibility of
    define which movies you already saw.

--- main.py
+++ main.py
@@ -5,9 +5,33 @@ def add_movie(name):
     movie_file = open("data/movies.txt", "a")
     movie_file.write(name + "\n")
 
-def list_movies():
-    movie_list = open("data/movies.txt","r").read()
+def list_movies(list_movie):
+    movie_list = open(list_movie,"r").read()
     return movie_list
+
+def seen_movie(movie):
+    movie_list = open("data/movies.txt","r")
+    movie_line = movie_list.readline()
+
+    new_movie_list = ""
+    found_movie = False
+
+    while movie_line:
+        if movie_line.find(movie) == -1:
+            new_movie_list += movie_line
+        elif not movie_line.find(movie):
+            found_movie = True
+        movie_line = movie_list.readline()
+
+    if found_movie:
+        movie_list = open("data/movies.txt", "w")
+        movie_list.write(new_movie_list)
+
+        movie_list = open("data/movies_seen.txt", "a")
+        movie_list.write(movie + "\n")
+
+    return found_movie
+
 
 ################################################
 ################################################
@@ -34,8 +58,22 @@ async def on_message(message):
 
         await message.add_reaction("👌")
 
-    match = re.search(r"ch.list", msg_content)
+    match = re.search(r"ch.list(.*)", msg_content)
     if match:
-        await message.channel.send(list_movies())
+        extra_param = match.group(1)
+        if extra_param == " seen":
+            await message.channel.send(list_movies("data/movies_seen.txt"))
+        else:
+            await message.channel.send(list_movies("data/movies.txt"))
 
-client.run("TOKEN")
+    match = re.search(r"ch.seen (.*)", msg_content)
+    if match:
+        movie = match.group(1)
+        could_seen = seen_movie(movie)
+
+        if could_seen:
+            await message.add_reaction("👌")
+        else:
+            await message.add_reaction("❌")
+
+client.run("")
